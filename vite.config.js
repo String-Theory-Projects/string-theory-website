62e1ed37b1000b3fe3731b670f6477ea42e37002
Yaqcodes <shuaibkhaleefa@gmail.com>
2026-04-17 16:48:28 +0100

    intermed iate commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  optimizeDeps: {
+    // cura-wasm uses threads.js workers internally; pre-bundling breaks worker resolution
+    exclude: ['cura-wasm', 'cura-wasm-definitions'],
+  },
 })
